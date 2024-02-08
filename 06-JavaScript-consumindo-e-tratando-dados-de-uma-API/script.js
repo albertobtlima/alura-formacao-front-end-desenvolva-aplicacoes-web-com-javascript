63b1c844ab073323ab03c99abca6625cb31bce5b
Alberto Lima <alberto.btlima@outlook.com>
2024-02-08 18:31:01 -0300

    Update
    Aula 04

--- 06-JavaScript-consumindo-e-tratando-dados-de-uma-API/script.js
+++ 06-JavaScript-consumindo-e-tratando-dados-de-uma-API/script.js
@@ -26,3 +26,29 @@ async function buscarEMostrarVideos(){
 }
 
 buscarEMostrarVideos();
+
+const barraDePesquisa = document.querySelector(".pesquisar__input");
+
+barraDePesquisa.addEventListener("input", filtrarPesquisa);
+
+function filtrarPesquisa() {
+    const videos = document.querySelectorAll(".videos__item");
+
+    if (barraDePesquisa.value != "") {
+        for (let video of videos) {
+            let titulo = video.querySelector(".titulo-video").textContent.toLowerCase();
+            let valorFiltro = barraDePesquisa.value.toLowerCase();
+
+            if (!titulo.includes(valorFiltro)) {
+                video.style.display = "none";
+            } else {
+                video.style.display = "block";
+            }
+        }
+    } else {
+        video.style.display = "block";
+    }
+}
+
+
+/* json-server --watch 06-JavaScript-consumindo-e-tratando-dados-de-uma-API/backend/videos.json */
